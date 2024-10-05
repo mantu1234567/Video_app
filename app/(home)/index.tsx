4869c0d91d9c3c735013447bc119358196e8a7bc
Mantu kumar <mantukumar803204@gmail.com>
2024-10-05 12:18:56 +0530

    310 time

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,14 +1,46 @@
 import { SignedIn, SignedOut, useAuth, useUser } from '@clerk/clerk-expo'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
-import { Link } from 'expo-router'
-import { useState } from 'react'
+import { Call, useStreamVideoClient } from '@stream-io/video-react-native-sdk';
+import { Link, useRouter } from 'expo-router'
+import { useEffect, useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native'
 import Dialog from "react-native-dialog";
+
 export default function IndexScreen() {
   const { user } = useUser();
+  const [calls, setCalls] = useState<Call[]>([]);
+  const router = useRouter();
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [isMyCalls, setIsMyCalls] = useState(false);
   const {signOut} = useAuth();
-
+   const client = useStreamVideoClient();
  const [dialogOpen,setDialogOpen] = useState(false);
+
+ const fetchCalls = async () => {
+  if (!client || !user) return;
+  const { calls } = await client.queryCalls({
+    filter_conditions: isMyCalls ? {
+          // filtercalls where is the create or a member of the call
+          $or: [
+            { created_by_user_id: user.id },
+
+            { members: { $in: [user.id] } },
+          ],
+        } : {},
+    sort: [{ field: "created_at", direction: -1 }],
+    watch: true,
+  });
+  //sort calls by participants count
+  const sortedCalls = calls.sort((a, b) => {
+    return b.state.participantCount - a.state.participantCount;
+  });
+  setCalls(sortedCalls);
+};
+
+ useEffect(() => {
+  fetchCalls();
+}, [isMyCalls])
+
   return (
     <View>
       <TouchableOpacity
@@ -36,7 +68,7 @@ export default function IndexScreen() {
     </Dialog.Container>
       <Text>Hello word</Text>
       <SignedIn>
-      <Text> You are Sign In</Text>
+      <Text> You are Sign In ghgh</Text>
       </SignedIn>
       
     </View>
